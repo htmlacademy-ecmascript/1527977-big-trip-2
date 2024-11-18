77180c9e2aea1e7991cf54398e31f2569ea15b25
Gorelikova Elena <lenagorelikova4560@gmail.com>
2024-11-18 14:56:53 +0300

    для проверки

--- src/const.js
+++ src/const.js
@@ -8,14 +8,14 @@ export const SORT_TYPES_BLOCK = ['event', 'offer'];
 
 export const FILTER_TYPES = ['everything', 'future', 'present', 'past'];
 
-export const MessageText = {
-  LOADING: 'Loading...',
-  LIST_EMPTY__EVERYTHING: 'Click New Event to create your first point',
-  LIST_EMPTY__PAST: 'There are no past events now',
-  LIST_EMPTY__PRESENT: 'There are no present events now',
-  LIST_EMPTY__FUTURE: 'There are no future events now',
-  FAIL: 'Failed to load latest route information',
-};
+// export const MessageText = {
+//   LOADING: 'Loading...',
+//   LIST_EMPTY__EVERYTHING: 'Click New Event to create your first point',
+//   LIST_EMPTY__PAST: 'There are no past events now',
+//   LIST_EMPTY__PRESENT: 'There are no present events now',
+//   LIST_EMPTY__FUTURE: 'There are no future events now',
+//   FAIL: 'Failed to load latest route information',
+// };
 
 export const FormatsDate = {
   DATE: 'MMM D',
